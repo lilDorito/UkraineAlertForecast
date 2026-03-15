c7574484d2cb6b5c93b73a9a6a5d07a3e9fc405d
lilDorito <mihailfedorec24@gmail.com>
2026-03-15 13:31:42 +0000

    hardcoded date for merge_once option

--- scripts/merge/merge_once.py
+++ scripts/merge/merge_once.py
@@ -18,6 +18,7 @@ PATHS = {
 
 if __name__ == "__main__":
     date_end = pd.Timestamp.now("UTC").tz_localize(None).floor("D") - pd.Timedelta(seconds=1)
+    # date_end = pd.Timestamp("2026-03-14") - pd.Timedelta(seconds=1)
 
     print("Building spine...")
     spine = build_spine(DATE_START, date_end)
